a3b6cd1b7d3a3149fba87eeb30a88bab5f3d1449
gayoung106 <xjongamx@kyonggi.ac.kr>
2024-02-05 21:29:19 +0900

    feat: 카드 추가 버튼

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="row">
-    <q-card v-for="c in 10" :key="c">
-      <div class="text-center bg-primary text-white">member</div>
-      <q-card-section>
-        <div>송가영</div>
-        <div>개발팀</div>
-        <div>000-0000-0000</div>
-      </q-card-section>
-    </q-card>
+  <div class="row justify-end q-pb-sm">
+    <q-btn dense icon="add" round color="positive" @click="addMember" />
+  </div>
+  <div class="row q-col-gutter-lg">
+    <div v-for="m in members" :key="m" class="col-6 col-md-4">
+      <Member :mid="m.mid" :name="m.name" :team="m.team" :contact="m.contact" />
+    </div>
   </div>
 </template>
+
+<script setup>
+import { reactive, defineAsyncComponent } from "vue";
+import { uid } from "quasar";
+
+const members = reactive([
+  { mid: uid(), name: "송가영", team: "개발팀", contact: "010-0000-0000" },
+  { mid: uid(), name: "아이유", team: "개발팀", contact: "010-0000-0000" },
+  { mid: uid(), name: "뷔", team: "개발팀", contact: "010-0000-0000" },
+]);
+
+const Member = defineAsyncComponent(() => import("./components/Member.vue"));
+
+const addMember = () => {
+  members.unshift({ mid: uid(), name: "", team: "", contact: "" });
+};
+</script>
